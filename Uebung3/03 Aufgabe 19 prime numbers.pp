54d099810a6b0c1c4e5bb01b9123deb16ff3856d
Alex <neodevlin@gmail.com>
2015-05-04 22:07:22 +0200

    Aufgabe 19 prime numbers

---
+++ Uebung3/03 Aufgabe 19 prime numbers.pp
@@ -0,0 +1,22 @@
+program Hello;
+var
+	primeSet:set of byte;
+	i,j:integer;
+	b:byte;
+begin
+  writeln ('Start');
+  primeSet := [2..255];
+  for i:=2 to 255 do
+  begin
+  	if i in primeSet then 
+  		begin
+  			j:=2;
+  			while ((i*j)<256) do 
+  			begin
+  				Exclude(primeSet,i*j);
+  				j:=j+1; 
+  			end;
+  		end;
+  end;
+  for b in primeSet do writeln(b);
+end.
